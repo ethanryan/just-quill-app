f04fbd137b0c33a8cbc481db0da55178e7b09c66
ethanryan <ethanryan@gmail.com>
2018-07-17 11:37:34 -0400

    successfully replacing icons with Font Awesome icons in QuillEditorSnow2

--- src/components/QuillEditorSnow2.js
+++ src/components/QuillEditorSnow2.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
 
 import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
@@ -17,6 +20,9 @@ import Italic from 'quill/formats/italic';
 import Header from 'quill/formats/header';
 
 import Icons from 'quill/ui/icons'; //can't seem to get the icons for the buttons...
+
+
+// var food = <FontAwesomeIcon icon="stroopwafel" />
 
 // require('https://cdn.quilljs.com/1.3.6/quill.snow.css')
 // require('https://cdn.quilljs.com/1.3.6/quill.js')
@@ -37,22 +43,43 @@ class QuillEditorSnow2 extends React.Component {
     });
 
     var icons = Quill.import('ui/icons');
-  icons['bold'] = '<i class="fa fa-bold" aria-hidden="true"></i>';
-  icons['italic'] = '<i class="fa fa-italic" aria-hidden="true"></i>';
-  icons['underline'] = '<i class="fa fa-underline" aria-hidden="true"></i>';
-  icons['image'] = '<i class="fa fa-picture-o" aria-hidden="true"></i>';
-  icons['code'] = '<i class="fa fa-code" aria-hidden="true"></i>';
+  // icons['dropdown'] = '<i class="fa fa-bold" aria-hidden="true"></i>';
+  icons['ql-picker-label'] = '<i class="fa fa-caret-down" aria-hidden="true">';
+
+    icons['bold'] = '<i class="fa fa-bold" aria-hidden="true"></i>';
+    icons['italic'] = '<i class="fa fa-italic" aria-hidden="true"></i>';
+    icons['underline'] = '<i class="fa fa-underline" aria-hidden="true"></i>';
+    icons['image'] = '<i class="fa fa-picture-o" aria-hidden="true"></i>';
+    icons['code'] = '<i class="fa fa-code" aria-hidden="true"></i>';
+    // icons['clean'] = '<i class="fa fa-code" aria-hidden="true"></i>'; //NOTE: icon for clean!
+    // icons['link'] = '<i class="fa fa-code" aria-hidden="true"></i>'; //NOTE: icon for link!
 
     var quill = new Quill('#editor', {
       theme: 'snow' //this needs to come after the above, which registers Snow...
     });
+
+    var dropdown = document.getElementsByClassName('ql-picker-label') //changing this here, can't do it in css...
+    console.log('dropdown is: ', dropdown)
+    dropdown[0].innerText = ""
+    console.log('dropdown is now: ', dropdown)
+
   } //componentDidMount
 
   render() {
     console.warn('0. QuillEditorSnow2 - render...')
     return (
       <div>
-        {/* <!-- Create the editor container --> */}
+
+        {/* <div>
+           Favorite Food:
+           {food}
+         </div>
+        <div>
+           Favorite drink:
+           <FontAwesomeIcon icon="coffee" />
+         </div> */}
+
+         {/* <!-- Create the editor container --> */}
         <div id="editor">
           <p>Hello World!</p>
           <p>Some initial <strong>bold</strong> text</p>
